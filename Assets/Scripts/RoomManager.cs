765148bcb112e69b1516fcb0652f9210d99744f9
gianpalumbo <gianpalumbo24@gmail.com>
2025-11-24 05:05:28 -0300

    mas decoracion y room manager

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -6,18 +6,33 @@ public class RoomManager : MonoBehaviour
 {
     [SerializeField] RoomComponent [] _rooms;
     [SerializeField] RoomComponent _initialRoom;
+    RoomComponent _actualRoom;
 
     private void Start()
     {
         foreach (var room in _rooms) room.gameObject.SetActive(false);
         _initialRoom.gameObject.SetActive(true);
-        foreach (var neighboor in _initialRoom._neighboors) neighboor.gameObject.SetActive(true);
+        foreach (var neighboor in _initialRoom.neighboors) neighboor.gameObject.SetActive(true);
+        _actualRoom = _initialRoom;
     }
 
-    public void RoomChange(RoomComponent newRoom)
+    private void Update()
     {
-        foreach (var room in _rooms) room.gameObject.SetActive(false);
+    }
+
+    public IEnumerator RoomChange(RoomComponent newRoom)
+    {
+        _actualRoom = newRoom;
+        foreach (var room in _rooms) 
+        { 
+            yield return null;
+            room.gameObject.SetActive(false);
+        }
         newRoom.gameObject.SetActive(true);
-        foreach (var neighboor in newRoom._neighboors) neighboor.gameObject.SetActive(true);
+        foreach (var neighboor in newRoom.neighboors) 
+        {
+            yield return null;
+            neighboor.gameObject.SetActive(true); 
+        }
     }
 }
