12fdbbffb94109300e2f7d3bdde8d9bd7642e30a
agusquindi7 <agustinquindimiltraduccion@gmail.com>
2025-11-25 02:03:21 -0300

    fix

--- Assets/Scripts/TriggerToCastle.cs
+++ Assets/Scripts/TriggerToCastle.cs
@@ -29,17 +29,9 @@ public class TriggerToCastle : MonoBehaviour
         {
             hasEntered = true;
 
-            StartCoroutine(DoFadeAndThenLoad());
+            ServiceLocator.Instance.GetDependency<PlayerMVC>().startingScene = PlayerMVC.StartingScene.Outside;
+            ServiceLocator.Instance.GetDependency<PlayerMVC>().LoadScene(PlayerMVC.StartingScene.Castle);
         }
     }
 
-    IEnumerator DoFadeAndThenLoad()
-    {
-        StartCoroutine(ServiceLocator.Instance.GetDependency<BlackPanelFade>().Fade(1f, .5f, false, false));
-
-        yield return new WaitForSeconds(1.5f);
-
-        ServiceLocator.Instance.GetDependency<PlayerMVC>().startingScene = PlayerMVC.StartingScene.Outside;
-        ServiceLocator.Instance.GetDependency<PlayerMVC>().LoadScene(PlayerMVC.StartingScene.Castle);
-    }
 }
